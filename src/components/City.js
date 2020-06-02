83fb8ea48e240032b1d0a9d2469171c8e88b186f
Ronan Lambert <rflambert@live.co.uk>
2020-06-02 19:01:18 +1200

    Updated to work with NZ city names, changed output to metric

--- src/components/City.js
+++ src/components/City.js
@@ -1,19 +1,12 @@
 import React, { useState } from 'react';
 
-function Zip(props) {
+function City(props) {
 
     const [validationError, setValidationError] = useState(null);
 
     const validate = (event) => {
-        const zipCodePattern = /^\d{5}$/;
-        const valid = zipCodePattern.test(event.target.value);
-        if (!valid) {
-            setValidationError('* should be a 5 digit number only');
-            props.clearResponse();
-        } else {
-            setValidationError('');
-            props.onZipChange(event.target.value);
-        }
+        setValidationError('');
+        props.onCityNameChange(event.target.value);
     };
 
     return (
@@ -30,7 +23,7 @@ function Zip(props) {
                         type="text" 
                         className="form-control" 
                         id="usr" 
-                        placeholder="US Zip Code (5 digit)"
+                        placeholder="NZ city name"
                         onKeyPress={(event) => {
                             if (event.key === "Enter") {
                                 validate(event);
@@ -46,4 +39,4 @@ function Zip(props) {
     );
 }
 
-export default Zip
+export default City
