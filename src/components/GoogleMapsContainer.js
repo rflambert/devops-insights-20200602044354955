653510611c43f0ad44c179a682109a9981b76d91
Ronan Lambert <rflambert@live.co.uk>
2020-06-04 15:20:21 +1200

    Added drop animation

--- src/components/GoogleMapsContainer.js
+++ src/components/GoogleMapsContainer.js
@@ -21,6 +21,7 @@ class GoogleMapsContainer extends Component {
                 lat: city.lat,
                 lng: city.lng
             }}
+            animation={this.props.google.maps.Animation.DROP}
             onClick={() => {this.props.onMarkerSelect(city.name)}}/>
         })
     }
